a5f9c67eadda8df7d0d7c556177a23d99d51309a
pitapit94 <141116486+pitapit94@users.noreply.github.com>
2023-08-06 18:00:52 +1200

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+    {
+        id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincerre@april.biz',
+    sayHi: () => console.log("hi")
+    },
+    {
+        id: 2,
+    name: 'Peter Yang',
+    username: 'Pyang',
+    email: 'peter@yang.biz'
+    },
+    {
+        id: 3,
+    name: 'Mark Churr',
+    username: 'Mchur',
+    email: 'mark@churr.biz'
+    },
+    {
+        id: 4,
+    name: 'Liz Liz',
+    username: 'Lliz',
+    email: 'liz@liz.biz'
+    },
+    {
+        id: 5,
+    name: 'Paul Graham',
+    username: 'Pgrahm',
+    email: 'Paul@Gramme.biz'
+    }
+]
