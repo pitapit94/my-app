bde50e457d3d4929a937f532e7e94d0368a3ae70
pitapit94 <peter.yang994@gmail.com>
2023-08-06 18:07:31 +1200

    test

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,7 +21,7 @@ class App extends React.Component{
 
     onSearchChange = (event) =>{
         this.setState({ searchfield: event.target.value})
-        console.log(event.target.value);
+        console.log(event.target);
         
     }    
 
